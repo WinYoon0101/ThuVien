420b7cf6bb3ab550d41b3adc42dcee400c0b1971
Uyên Nguyễn <22521641@gm.uit.edu.vn>
2024-05-21 23:53:28 +0700

    Update Form_6.java

--- ui-dashboard-001/src/com/raven/form/Form_6.java
+++ ui-dashboard-001/src/com/raven/form/Form_6.java
@@ -32,7 +32,7 @@ public class Form_6 extends javax.swing.JPanel {
     ResultSet rs = null;
     public void updateDB () {
         try {
-            conn = DriverManager.getConnection("jdbc:oracle:thin:@HacThienCau:1521:ORCLDB", "C##UITthuvien", "uitthuvien");
+            conn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.56.1:1521:orcldb", "C##UITthuvien", "uitthuvien");
             String sql = "SELECT * FROM PHIEUTHU";
             ps = conn.prepareStatement(sql);
             
@@ -378,7 +378,7 @@ public class Form_6 extends javax.swing.JPanel {
     
     try {
         // Connect to the database
-        conn = DriverManager.getConnection("jdbc:oracle:thin:@HacThienCau:1521:ORCLDB", "C##UITthuvien", "uitthuvien");
+        conn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.56.1:1521:orcldb", "C##UITthuvien", "uitthuvien");
 
 
         
@@ -463,7 +463,7 @@ public class Form_6 extends javax.swing.JPanel {
 
     try {
         // Update the user information in the database
-        conn = DriverManager.getConnection("jdbc:oracle:thin:@HacThienCau:1521:ORCLDB", "C##UITthuvien", "uitthuvien");
+        conn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.56.1:1521:orcldb", "C##UITthuvien", "uitthuvien");
         String sql = "UPDATE PHIEUTHU SET MAPM = ?, MADG = ?, MAND = ?, TONGNO = ?, TIENTHU = ?, TIENCONLAI = ?, TGPT = ? WHERE MAPT = ?";
         ps = conn.prepareStatement(sql);
         ps.setDouble(1, Double.parseDouble(mapm));
@@ -518,7 +518,7 @@ public class Form_6 extends javax.swing.JPanel {
     if (option == JOptionPane.YES_OPTION) {
         try {
             // Delete the user from the database
-            conn = DriverManager.getConnection("jdbc:oracle:thin:@HacThienCau:1521:ORCLDB", "C##UITthuvien", "uitthuvien");
+            conn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.56.1:1521:orcldb", "C##UITthuvien", "uitthuvien");
             String sql = "DELETE FROM PHIEUTHU WHERE MAPT = ?";
             ps = conn.prepareStatement(sql);
             ps.setString(1, maPT);
